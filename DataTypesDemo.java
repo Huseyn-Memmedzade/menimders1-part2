9a9e754394105d93d5e229ff62af653a8807363b
Backend Developer <hseyn.mmmdzad@bk.ru>
2025-07-10 15:45:55 +0400

    Add files via upload

---
+++ DataTypesDemo.java
@@ -0,0 +1,29 @@
+package yenipabka;
+
+public class DataTypesDemo {
+    public static void main(String[] args) {
+
+        int a = 10;
+        double b = 10.5;
+        long c = 10L;
+        float d = 10.6F;
+        String Salam = "Salamlar";
+        char e = 'e';
+        boolean t = true;
+        boolean f = false;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(Salam);
+        System.out.println(e);
+        System.out.println(t);
+        System.out.println(f);
+    }
+}
+// Tapşırıq 2: Dəyişənlər və Məlumat Tipləri
+// Bir DataTypesDemo adlı sinif yaradın.
+// Müxtəlif növ dəyişənləri elan edin və ilkin dəyər verin — məsələn, bir tam ədəd (integer), bir onluq ədəd (double) və bir mətn (string).
+// Bu dəyişənlərin dəyərlərini konsola çap edin.
+// Data Tipler qeyd olundu ve deyerleri capa verildi✅
